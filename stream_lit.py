3770691478a7b543fc83b0f233898a6e84bc2e01
ch1ragg <154981474+ch1ragg@users.noreply.github.com>
2023-12-27 17:15:24 +0530

    Update stream_lit.py
    added header
    added text

--- stream_lit.py
+++ stream_lit.py
@@ -1,3 +1,8 @@
 import streamlit 
 
 streamlit.title("My parents healthy dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Blueberry Oat Meal")
+streamlit.text("Strawberry Cornflakes")
+streamlit.text("Milk Chocos")
+
